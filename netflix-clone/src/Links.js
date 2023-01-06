1282687554e20b11b7cd539808954914a8e9a97e
Hemanth's Macbook Pro <apple@Apples-MacBook-Pro-2.local>
2023-01-06 11:53:34 +0530

    nav links added

---
+++ netflix-clone/src/Links.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Links.css';
+
+const Links = () => {
+  return (
+    <div className="nav-links">
+      <a href="" >Home</a>
+      <a href="">TV Shows</a>
+      <a href="">New & Popular</a>
+      <a href="">My List</a>
+      <a href="">Browse by Language</a>
+    </div>
+  );
+};
+
+export default Links;
--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -30,6 +30,14 @@ const Nav = () => {
           alt=""
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1280px-Netflix_2015_logo.svg.png"
         />
+        
+        <div className="nav-links">
+      <a href="" >Home</a>
+      <a href="">TV Shows</a>
+      <a href="">New & Popular</a>
+      <a href="">My List</a>
+      <a href="">Browse by Language</a>
+    </div>
         <img
           className="nav_avatar"
           alt=""
